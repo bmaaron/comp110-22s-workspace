1f0776f167550bc4c7b0b795ffc286aee94d26ca
Bryce Aaron <baaron@unc.edu>
2022-01-19 16:17:54 -0500

    starting over on turtle project-- need to learn

--- projects/turtle_project.py
+++
@@ -1,9 +0,0 @@
-"""Turtle Project attempt."""
-
-__author__ = 730530687
-
-from turtle import Turtle, colormode, done
-
-def main() -> None:
-    """This is the endpoint of my scene."""
-    #TODO: de
