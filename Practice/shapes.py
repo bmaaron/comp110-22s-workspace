7419247af0203d152e6dc83da14d62933f23966c
Bryce Aaron <baaron@unc.edu>
2022-01-19 23:55:53 -0500

    sending conditionals, input, and  personal practic

---
+++ Practice/shapes.py
@@ -0,0 +1,3 @@
+name = input("Enter your name: ")
+mood = input("How are you feeling? ") 
+print("Hello " + name + "! " + "I'm glad to see you are doing " + mood + "!!!")
---
+++ lessons/input.py
@@ -0,0 +1,8 @@
+"""LS06- Demonstrates asking the user for input."""
+
+__author__ = 730530687
+
+user_name: str = input("What is your name? ")
+print("Hello, " + user_name + ", good morning!")
+print("You are awesome, " + user_name + ".")
+print("I hope you have an amazing day " + user_name + "!!!!!!!!!")
