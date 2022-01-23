b0280943edc3fcdd9081ae65977ffc10726fd189
Bryce Aaron <baaron@unc.edu>
2022-01-23 15:27:46 -0500

    Checking Part 1 for correctness

---
+++ exercises/ex01/ex01_chardle.py
@@ -0,0 +1,9 @@
+"""EX01 Chardle Project."""
+
+__author__ = 730530687
+
+WORD: str = input("Enter a 5-character word: ")
+
+LETTER: str = input("Enter a single chatacter:")
+
+print("Searching for " + LETTER + " in " + WORD)
