fecd01f9673114bdd40c7647071d77b052e09790
Bryce Aaron <baaron@unc.edu>
2022-01-24 18:09:38 -0500

    part 2 & 3 stages complete, checking work

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,35 @@
+"""EX01 Chardle Project."""
+
+__author__ = "730530687"
+w: str = input("Enter a 5-character word: ")  # w = word
+character: str = input("Enter a single chatacter: ")
+print("Searching for " + character + " in " + w)
+count: int = 0
+
+if character == w[0]:
+    print(character + " found at index 0")
+    count = count + 1
+
+if character == w[1]:
+    print(character + " found at index 1")
+    count = count + 1
+    
+if character == w[2]:
+    print(character + " found at index 2")
+    count = count + 1
+
+if character == w[3]:
+    print(character + " found at index 3")
+    count = count + 1
+
+if character == w[4]:
+    print(character + " found at index 4")
+    count = count + 1
+
+if count == 0:
+    print("No instances of " + character + " found in " + w)
+
+if count == 1:
+    print(str(count) + " instance of " + character + " found in " + w)
+if count > 1:
+    print(str(count) + " instances of " + character + " found in " + w)
