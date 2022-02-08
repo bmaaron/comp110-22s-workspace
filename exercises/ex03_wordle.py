b6cd75b8279f8638268bcc57677479b2c4a104b9
Bryce Aaron <baaron@unc.edu>
2022-02-07 20:31:29 -0500

    final ex03 going to backup

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -2,17 +2,12 @@
 
 __author__ = "730530687"
 
-# secret: str = "codes"  
-# secret_len: int = len(secret)
-# guess: str = input(f"Enter a {secret_len} word: ")  
-
-
+  
 def contains_char(guess: str, char_in_guess: str) -> bool:
     """Searches for character in guess word."""
     exist: bool = False
     assert len(char_in_guess) == 1
-    i: int = 0
-
+    i: int = 0 
     while not exist and i < len(guess):
         if guess[i] == char_in_guess:
             exist = True
@@ -29,5 +24,53 @@ def emojified(guess: str, secret: str) -> str:
     WHITE_BOX: str = "\U00002B1C"
     GREEN_BOX: str = "\U0001F7E9"
     YELLOW_BOX: str = "\U0001F7E8"
+    answer: str = ""
+    i: int = 0  
+    secret_len: int = len(secret)
 
-    
+    while i < secret_len:
+        if secret[i] == guess[i]:
+            answer = answer + GREEN_BOX 
+            
+        else: 
+            exist: bool = False
+            if contains_char(secret, guess[i]):
+                exist = True
+            if exist:
+                answer = answer + YELLOW_BOX
+            else:
+                answer = answer + WHITE_BOX
+        i = i + 1
+
+    return answer 
+
+
+def input_guess(expected_length: int) -> str:
+    """Give integer an expected length of a guess as a parameter."""
+    guess: str = input(f"Enter a {expected_length} character word: ")  
+    while len(guess) != expected_length:
+        guess = input(f"That wasn't {expected_length} chars! Try again: ")
+        
+    return guess
+
+
+def main() -> None:
+    """The entrypoint of the program and main game loop."""
+    x: int = 1
+    secret: str = "codes"
+    secret_len: int = len(secret)
+    guess: str = "" 
+    while x <= 6 and guess != secret: 
+        print(f"=== Turn {x}/6 ===")
+        guess = input_guess(secret_len)
+        print(emojified(guess, secret))
+        x = x + 1
+
+    if guess == secret:
+        print(f"You won in {x - 1}/6 turns!")
+    else: 
+        print("X/6 - Sorry, try again tomorrow!")
+        
+
+if __name__ == "__main__":
+    main()
